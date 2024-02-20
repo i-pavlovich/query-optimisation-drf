e118cbcc7018314486c5efe3031beb680652deef
Ivan Pavlovich <ivan.pavlovich.business@gmail.com>
2024-02-20 23:24:41 +0300

    feat: add view classes

--- src/api/views.py
+++ src/api/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Lesson, Product, UserAccessibleProduct, UserLessonStatistics
+from .serializers import (
+    ProductStatisticsSerializer,
+    UserLessonStatisticsSerializer,
+)
+
+
+class AccessibleProductsLessonListApiView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserLessonStatisticsSerializer
+
+    def get_queryset(self):
+        user_id = self.request.user.id
+        accessible_products = UserAccessibleProduct.objects.filter(user=user_id).values(
+            "product"
+        )
+        lessons = Lesson.objects.filter(product__in=accessible_products).values("id")
+        queryset = UserLessonStatistics.objects.filter(
+            user=user_id, lesson__in=lessons
+        ).select_related("lesson")
+        return queryset
+
+
+class ProductLessonListApiView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = UserLessonStatisticsSerializer
+
+    def get_queryset(self):
+        user_id = self.request.user.id
+        product_id = self.kwargs["product_id"]
+        lessons = Lesson.objects.filter(product=product_id).values("id")
+        queryset = UserLessonStatistics.objects.filter(
+            user=user_id, lesson__in=lessons
+        ).select_related("lesson")
+        return queryset
+
+
+class ProductStatisticsListApiView(generics.ListAPIView):
+    serializer_class = ProductStatisticsSerializer
+    queryset = Product.objects.all().select_related("author")
